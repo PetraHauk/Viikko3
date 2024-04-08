930bd6ecc430f93c2d6637e3a1995d43a9f9c306
Petra Haukkala <petra.haukkala@gmail.com>
2024-04-08 11:50:14 +0300

    Branch commit

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+const hostname = '127.0.0.1';
+const app = express();
+const port = 3000;
+
+app.get('/api/v1/cat', (req, res) => {
+   const cat = {
+       cat_id: 1,
+       name: 'Viski',
+       birthdate: '2034-02-02',
+       weight: 12,
+       owner: 3,
+       image: 'https://loremflickr.com/320/240/cat'
+   };
+   res.json(cat);
+});
+
+app.use('/public', express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my REST API!');
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
